da50e21b21c372f1442bd79de66596d46954e644
yevhen.shevtsov <evs062009@gmail.com>
2019-10-28 21:11:05 +0200

    get(), remove(), containsKey() finished

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -6,18 +6,27 @@ public class LongMapImpl<V> implements LongMap<V> {
     private Entry[] table;
     private int maxLoop;
     private long size;
+    private double topLoadFactor;
+    private double bottomLoadFactor;
 
     public LongMapImpl() {
         capacity = 4;
         table = new Entry[capacity];
         maxLoop = Math.min(table.length, 20);
         size = 0;
+        topLoadFactor = 0.8;
+        bottomLoadFactor = 0.6;
     }
 
     public V put(long key, V value) {
         int potentialIndex = -1;
+
+        if ((double) size / capacity > topLoadFactor) {
+            increaseTable();
+        }
+
         for (int i = 0; i < maxLoop; i++) {
-            int index = getIndex(i);
+            int index = getIndex(key, i);
 
             if (table[index] == null) {
                 if (potentialIndex == -1) {
@@ -30,20 +39,42 @@ public class LongMapImpl<V> implements LongMap<V> {
                     }
                 } else if (table[index].key == key) {
                     table[index].value = value;
+                    size++;
+                    return value;
                 }
             }
         }
 
-        size++;
+        if (potentialIndex != -1) {
+            table[potentialIndex] = new Entry<>(key, value);
+            size++;
+            return value;
+        }
+        return null;
     }
 
     public V get(long key) {
-
+        int index = getIndex2(key);
+        if (index != -1) {
+            return (V) table[index].value;
+        }
+        return null;
     }
 
     public V remove(long key) {
+        V value = null;
+        int index = getIndex2(key);
+        if (index != -1) {
+            value = (V) table[index].value;
+            table[index].value = null;
+            table[index].zeroForDeletedEntry = 0;
+            size--;
 
-        size--;
+            if ((double) size / capacity < bottomLoadFactor) {
+                decreaseTable();
+            }
+        }
+        return value;
     }
 
     public boolean isEmpty() {
@@ -51,7 +82,7 @@ public class LongMapImpl<V> implements LongMap<V> {
     }
 
     public boolean containsKey(long key) {
-
+        return getIndex2(key) != -1;
     }
 
     public boolean containsValue(V value) {
@@ -74,9 +105,22 @@ public class LongMapImpl<V> implements LongMap<V> {
         111
     }
 
-    private int getIndex(int p) {
+    //XXX Correct method name
+    private int getIndex2(long key) {
+        for (int i = 0; i < maxLoop; i++) {
+            int index = getIndex(key, i);
+            if (table[index] != null && table[index].zeroForDeletedEntry == 1
+                    && table[index].key == key) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    private int getIndex(long key, int p) {
 
     }
+
 
     private static class Entry<V> {
         private long key;
