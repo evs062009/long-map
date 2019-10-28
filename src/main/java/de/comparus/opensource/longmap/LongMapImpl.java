e61405cd75df6139638571f912093d942b4b3b3a
yevhen.shevtsov <evs062009@gmail.com>
2019-10-28 16:22:48 +0200

    some minor changing

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -1,43 +1,92 @@
 package de.comparus.opensource.longmap;
 
 public class LongMapImpl<V> implements LongMap<V> {
+
+    private int capacity;
+    private Entry[] table;
+    private int maxLoop;
+    private long size;
+
+    public LongMapImpl() {
+        capacity = 4;
+        table = new Entry[capacity];
+        maxLoop = Math.min(table.length, 20);
+        size = 0;
+    }
+
     public V put(long key, V value) {
-        return null;
+        int potentialIndex = -1;
+        for (int i = 0; i < maxLoop; i++) {
+            int index = getIndex(i);
+
+            if (table[index] == null) {
+                if (potentialIndex == -1) {
+                    potentialIndex = index;
+                }
+            } else {
+                if (table[index].zeroForDeletedEntry == 0) {
+                    if (potentialIndex == -1) {
+                        potentialIndex = index;
+                    }
+                } else if (table[index].key == key) {
+                    table[index].value = value;
+                }
+            }
+        }
+
+        size++;
     }
 
     public V get(long key) {
-        return null;
+
     }
 
     public V remove(long key) {
-        return null;
+
+        size--;
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public boolean containsKey(long key) {
-        return false;
+
     }
 
     public boolean containsValue(V value) {
-        return false;
+
     }
 
     public long[] keys() {
-        return null;
+
     }
 
     public V[] values() {
-        return null;
+
     }
 
     public long size() {
-        return 0;
+        return size;
     }
 
     public void clear() {
+        111
+    }
 
+    private int getIndex(int p) {
+
+    }
+
+    private static class Entry<V> {
+        private long key;
+        private V value;
+        private byte zeroForDeletedEntry;         // == 0, when entry marked as deleted
+
+        public Entry(long key, V value) {
+            this.key = key;
+            this.value = value;
+            this.zeroForDeletedEntry = 1;
+        }
     }
 }
