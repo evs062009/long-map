3a93e9a9667dfdb10f01338c12d9f6054dfc7c05
yevhen.shevtsov <evs062009@gmail.com>
2019-10-29 15:52:07 +0200

    reserve creation started

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -1,39 +1,122 @@
 package de.comparus.opensource.longmap;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class LongMapImpl<V> implements LongMap<V> {
 
-    private int capacity;
     private Entry[] table;
-    private int maxLoop;
+    private int capacity;
     private int size;
+    private int reserveSize;
+    private int maxLoop;
     private double topLoadFactor;
     private double bottomLoadFactor;
 
+    private static final int DEFAULT_CAPACITY = 8;
+    private static final int MAX_CAPACITY = 1 << 30;
+
     public LongMapImpl() {
-        capacity = 4;
+        capacity = DEFAULT_CAPACITY;
         table = new Entry[capacity];
         maxLoop = Math.min(table.length, 20);
         size = 0;
-        topLoadFactor = 0.8;
-        bottomLoadFactor = 0.6;
+        topLoadFactor = 0.9;
+        bottomLoadFactor = 0.4;
     }
 
-    //XXX debug - decrease
     public V put(long key, V value) {
-        int potentialIndex = -1;
-
         if (value == null) {
             throw new NullPointerException("value = null");
         }
-        if ((double) size / capacity > topLoadFactor) {
+        if ((double) (size + 1) / capacity > topLoadFactor) {
             increaseTable();
         }
 
+        return putNewPair(key, value);
+    }
+
+    public V get(long key) {
+        int index = getKeyIndex(key);
+        if (index != -1) {
+            return (V) table[index].value;
+        }
+        return getFromReserve(key);
+    }
+
+    public V remove(long key) {
+        V value;
+        int index = getKeyIndex(key);
+        if (index != -1) {
+            value = remove(index);
+        } else {
+            value = removeInReserve(key);
+        }
+        if ((double) size / capacity < bottomLoadFactor) {
+            decreaseTable();
+        }
+        return value;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean containsKey(long key) {
+        return getKeyIndex(key) != -1 || containsKeyInReserve();
+    }
+
+    public boolean containsValue(V value) {
+        if (value == null) {
+            throw new NullPointerException("value = null");
+        }
+        for (Entry entry : table) {
+            if (entry != null && value.equals(entry.value) && entry.zeroForDeletedEntry == 1) {
+                return true;
+            }
+        }
+        return containsValueInReserve(value);
+    }
+
+    public long[] keys() {
+        if (size == 0) {
+            return new long[0];
+        } else {
+            LongStream tableKeys = Arrays.stream(table).filter(isEntryNotEmpty()).mapToLong(Entry::getKey);
+            LongStream reserveKeys = getReserveKeys();
+            return LongStream.concat(tableKeys, reserveKeys).toArray();
+        }
+    }
+
+    public V[] values() {
+        if (size == 0) {
+            return (V[]) new Object[0];
+        } else {
+            return (V[]) Arrays.stream(table).filter(isEntryNotEmpty()).map(Entry::getValue).toArray();
+            111
+        }
+    }
+
+    public long size() {
+        return size;
+    }
+
+    public void clear() {
+        clearTable();
+        capacity = DEFAULT_CAPACITY;
+        table = new Entry[capacity];
+        clearReserve();
+        size = 0;
+    }
+
+
+    private V putNewPair(long key, V value) {
+        int potentialIndex = -1;
+
         for (int i = 0; i < maxLoop; i++) {
-            int index = getIndex(key, i);
+            int index = calculateIndex(key, i);
 
             if (table[index] == null) {
                 if (potentialIndex == -1) {
@@ -56,84 +139,41 @@ public class LongMapImpl<V> implements LongMap<V> {
             size++;
             return value;
         }
-        return null;
+        return putToReserve(key, value);
     }
 
-    public V get(long key) {
-        int index = getIndex2(key);
-        if (index != -1) {
-            return (V) table[index].value;
-        }
-        return null;
-    }
-
-    public V remove(long key) {
-        V value = null;
-        int index = getIndex2(key);
-        if (index != -1) {
-            value = (V) table[index].value;
-            table[index].value = null;
-            table[index].zeroForDeletedEntry = 0;
-            size--;
-
-            if ((double) size / capacity < bottomLoadFactor) {
-                decreaseTable();
-            }
-        }
+    private V remove(int index) {
+        V value = (V) table[index].value;
+        table[index].value = null;
+        table[index].zeroForDeletedEntry = 0;
+        size--;
         return value;
     }
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public boolean containsKey(long key) {
-        return getIndex2(key) != -1;
-    }
-
-    public boolean containsValue(V value) {
-        if (value == null) {
-            throw new NullPointerException("value = null");
-        }
-
-        for (Entry entry : table) {
-            if (entry != null && value.equals(entry.value) && entry.zeroForDeletedEntry == 1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public long[] keys() {
-        if (size == 0) {
-            return new long[0];
-        } else {
-            return Arrays.stream(table).filter(e -> e != null && e.zeroForDeletedEntry == 1)
-                    .mapToLong(Entry::getKey).toArray();
+    private void increaseTable() {
+        if (capacity < MAX_CAPACITY) {
+            capacity = capacity << 1;
+            reHash();
         }
     }
 
-    public V[] values() {
-        if (size == 0) {
-            return (V[]) new Object[0];
-        } else {
-            return (V[]) Arrays.stream(table).filter(e -> e != null && e.zeroForDeletedEntry == 1)
-                    .map(Entry::getValue).toArray();
+    private void decreaseTable() {
+        if (capacity > DEFAULT_CAPACITY) {
+            capacity = capacity >> 1;
+            reHash();
         }
     }
 
-    public long size() {
-        return size;
+    private void reHash() {
+        Entry[] newTable = new Entry[capacity];
+        Arrays.stream(table).filter(isEntryNotEmpty()).forEach(e -> putForRehash(e, newTable));
+        clearTable();
+        table = newTable;
     }
 
-    public void clear() {
-        111
-    }
-
-    //XXX Correct method name
-    private int getIndex2(long key) {
+    private int getKeyIndex(long key) {
         for (int i = 0; i < maxLoop; i++) {
-            int index = getIndex(key, i);
+            int index = calculateIndex(key, i);
             if (table[index] != null && table[index].zeroForDeletedEntry == 1
                     && table[index].key == key) {
                 return index;
@@ -142,27 +182,80 @@ public class LongMapImpl<V> implements LongMap<V> {
         return -1;
     }
 
-    private int getIndex(long key, int p) {
+    private int calculateIndex(long key, int i) {
+        int hash = getHash(key);
+        int h1 = hash % capacity;
+        int h2 = 1 + (hash % (capacity - 1));
+        return (h1 + i * h2) % 13;
+    }
+
+    private int getHash(long key) {
+        return (int) (key ^ (key >>> 32));
+    }
+
+    private void putForRehash(Entry entry, Entry[] table) {
+        for (int i = 0; i < maxLoop; i++) {
+            int index = calculateIndex(entry.key, i);
+            if (table[index] == null || table[index].zeroForDeletedEntry == 0) {
+                table[index] = entry;
+                return;
+            }
+        }
+        if (putToReserve(entry.key, (V) entry.value) == null) {
+            throw new RuntimeException("Error in rehash.");
+        }
+    }
+
+    private V putToReserve(long key, V value) {
+        111
+    }
+
+    private void clearTable() {
+        for (int i = 0; i < capacity; i++) {
+            table[i] = null;
+        }
+    }
+
+    private Predicate<Entry> isEntryNotEmpty() {
+        return e -> e != null && e.zeroForDeletedEntry == 1;
+    }
+
+    private V getFromReserve(long key) {
+    }
+
+    private V removeInReserve(long key) {
+    }
+
+    private boolean containsKeyInReserve() {
+    }
+
+    private boolean containsValueInReserve(V value) {
+    }
+
+    private LongStream getReserveKeys() {
 
     }
 
+    private void clearReserve() {
+        111
+    }
 
     private static class Entry<V> {
         private long key;
         private V value;
         private byte zeroForDeletedEntry;         // == 0, when entry marked as deleted
 
-        public Entry(long key, V value) {
+        private Entry(long key, V value) {
             this.key = key;
             this.value = value;
             this.zeroForDeletedEntry = 1;
         }
 
-        public long getKey() {
+        private long getKey() {
             return key;
         }
 
-        public V getValue() {
+        private V getValue() {
             return value;
         }
     }
